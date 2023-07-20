b77793fa39b8635b65576ed5bc383eb91153303b
cartercodes <carter.svo@gmail.com>
2023-07-20 00:24:06 -0400

    added downloadable resume to sidebar navigation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,6 +9,7 @@ import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import reactLogo from '../assets/signature.png';
 import { Link } from 'react-scroll';
+import Resume from '../assets/Tech-Resume.pdf';
 
 const Navbar: React.FC = () => {
     const [nav, setNav] = useState(false);
@@ -124,7 +125,7 @@ const Navbar: React.FC = () => {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                         <a
                             className='flex justify-between items-center w-full text-gray-300'
-                            //href=
+                            href={Resume}
                         >
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
