5064abcca1aa9e1821efda7b80dee9cdeec07de8
cartercodes <carter.svo@gmail.com>
2024-03-06 11:46:54 -0500

    updated nav links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -100,7 +100,7 @@ const Navbar: React.FC = () => {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                         <a
                             className='flex justify-between items-center w-full text-gray-300'
-                            href='https://www.linkedin.com/in/mitchellcartersfg/'
+                            href='https://www.linkedin.com/in/mitchcarterd/'
                         >
                             Linkedin <FaLinkedin size={30} />
                         </a>
