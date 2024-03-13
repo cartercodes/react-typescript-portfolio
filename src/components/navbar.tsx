ff48cd6bd77504d6ad68b7249f73cd3bcd847c42
cartercodes <carter.svo@gmail.com>
2024-03-12 22:22:55 -0400

    updated resume to pdf to download

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,7 +8,7 @@ import {
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import reactLogo from '../assets/signature.png';
 import { Link } from 'react-scroll';
-import Resume from '../assets/carter_resume.pdf';
+import Resume from '../assets/ATS.pdf';
 
 const Navbar: React.FC = () => {
     const [nav, setNav] = useState(false);
